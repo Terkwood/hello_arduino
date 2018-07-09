2560a43bde48d4ca12e985c84fdb26cdfecfa7bc
Terkwood <metaterkhorn@gmail.com>
2018-07-09 14:10:57 -0400

    draft

--- piezo_temp/piezo_temp.ino
+++ piezo_temp/piezo_temp.ino
@@ -1,5 +1,14 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
 #define TEMP_PIN      2
 #define PIEZO_PIN     3    // PWM-capable pin
+
+// create oneWire instance to talk to any OneWire devices.
+// in our case, we only have an 1820B temp sensor
+OneWire oneWire(TEMP_PIN);
+// inform dallas temp of our oneWire reference
+DallasTemperature sensors(&oneWire);
 
 #define WAIT        100    // millis between beeps
 
@@ -16,11 +25,22 @@ float temp_celsius = 0;
 
 void setup() {
   pinMode(PIEZO_PIN, OUTPUT);
+  
+  // init serial port
+  Serial.begin(9600);
+  Serial.println("Hello Sensor World");
+  
+  // start up dallas temp library
+  sensors.begin();
 }
 
 void loop() {
-  analogWrite(PIEZO_PIN, 128);  // 50 percent duty cycle tone to the piezo
+  sensors.requestTemperatures(); 
+  Serial.print("Temp: ");
+  Serial.print(sensors.getTempCByIndex(0)); // 0 is the first IC on the bus
+  
+  /*analogWrite(PIEZO_PIN, 128);  // 50 percent duty cycle tone to the piezo
   delay(WAIT);
   digitalWrite(PIEZO_PIN, LOW);
-  delay(WAIT);
+  delay(WAIT);*/
 }
